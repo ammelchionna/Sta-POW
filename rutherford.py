368dc221b9207beae2b85bb06786dcaa5a33bff7
Adam Hayes <abraunhayes@gmail.com>
2013-04-30 18:03:57 -0400

    Energy loss accounted for..BUG: rutherford c.s. not correct * subtracts the energy loss from face to center first * assumes that interaction is in the center of the target * accounts for energy loss after scattering, angle dependent

--- rutherford.py
+++ rutherford.py
@@ -22,20 +22,81 @@ import numpy
 import matplotlib.pyplot as plt
 import pylab
 import sys
+import subprocess
+
+# target_thickness [mg/cm^2]
+
+
+def get_floats(prompt_string = ''):
+    temp_string = raw_input(prompt_string)
+    temp_string = temp_string.replace(',',' ')
+    temp_list   = temp_string.split()
+    return [float(i) for i in temp_list]
+
+def run_elast(target_atomic_number,target_mass,projectile_atomic_number,projectile_mass,beam_energy,target_thickness):
+
+    # The executable with path:
+    elast_command = '/home/adam/Rachel/elast_source/elast'
+
+    output_file_name = 'temp_rutherford_file.txt'
+    initial_command_line_options = "-Qa 0 lp"
+
+    target_string       = "\"1(" + str(int(target_atomic_number)) + "," + str(int(target_mass)) + ")\"" # Leading "1" means one component in target.
+    thickness_string    = str(target_thickness)
+    projectile_string   = "\"(" + str(int(projectile_atomic_number)) + "," + str(int(projectile_mass)) + ")\""
+    beam_energy_string  = format(beam_energy,".3f")  # rounded to the nearest keV.
+    command_line        = elast_command + " " + initial_command_line_options + " " + target_string + " " +\
+                          thickness_string + " " + projectile_string + " " + beam_energy_string
+
+    command_line += ' > ' + output_file_name
+
+    with open("garbage","w") as garbage_file:
+        subprocess.call(command_line,shell=True,stderr=garbage_file)  # (Redirect errors to the garbage file.)
+
+    # Read the elast output.
+    with open(output_file_name,'r') as output_file:
+        file_lines = output_file.readlines()
+
+    if len(file_lines) > 1:
+        return None, None
+
+    fields = file_lines[0].split()
+    energy_loss    = float(fields[0])
+    stopping_power = float(fields[1])
+
+    return stopping_power, energy_loss
 
 # Check to see if the user gave command line arguments.
 if len(sys.argv) == 9:
     interactive = False
+    target_thickness = None
     all_args = sys.argv[1:9]
     z1,a1,z2,a2,amin,amax,astep,e0 = [float(x) for x in all_args]
+if len(sys.argv) == 10:
+    # User included a target thickness to calculate exit energies.
+    print 'Note: the interaction is assumed to be at the target center.'
+    interactive = False
+    all_args = sys.argv[1:10]
+    z1,a1,z2,a2,amin,amax,astep,e0,target_thickness = [float(x) for x in all_args]
 else:
-    print 'Command-line usage:\nrutherford.py Zp Ap Zt At theta_min theta_max theta_step beam_energy'
+    print 'Command-line usage:\nrutherford.py Zp Ap Zt At theta_min theta_max theta_step beam_energy [target_thickness]'
+    print '  Target thickness is optional.'
     print '  e.g. rutherford.py 54 136 74 186 10 170 1 800 > out.txt'
     print '                     Zp  Ap Zt  At |angles| Ebeam [MeV] (redirected to file "out.txt".'
     interactive = True
+
+if not target_thickness == None:
+
+    # Calculate the energy at the center of the target.
+    dummy, energy_loss = run_elast(z2,a2,z1,a1,e0,target_thickness / 2.0)
+
+    # Use the energy at the center of the target instead of the beam energy.
+    e0 = e0 - energy_loss
+
+
+
 # cnt is the angle of a normal from the target position to a planar detector.
 # disf is the distance along that normal from target to detector.
-
 cnt  = 49.0  # degrees
 disf = 12.8  # cm
 
@@ -45,11 +106,7 @@ const2 = math.sin(math.radians(cnt))
 consta = math.cos(math.radians(cnt + 90.))
 constb = math.sin(math.radians(cnt + 90.))
 
-def get_floats(prompt_string = ''):
-    temp_string = raw_input(prompt_string)
-    temp_string = temp_string.replace(',',' ')
-    temp_list   = temp_string.split()
-    return [float(i) for i in temp_list]
+
 
 if interactive:
     z1,a1,z2,a2 = get_floats('Enter Zp,Ap (projectile), Zt,At (target) : ')
@@ -83,10 +140,15 @@ print 'Maximum scattering angle in lab frame is ' + str(thelim) + ' degrees.'
 print 'Safe distance is ' + str(dsafe) + ' fm.'
 
 header = [
-        'theta'.rjust(6) + 'theta'.rjust(8) + 'theta'.rjust(8)  + 'dist of'.rjust(11)   +  'energy'.rjust(8) + 'dsigma/domega'.rjust(15) + 'dsigma/domega'.rjust(15) +  'dsigma/domega'.rjust(15) + 'beta'.rjust(8) + 'beta'.rjust(8) + 'time of flight'.rjust(16),
-        'scat '.rjust(6) + 'rec  '.rjust(8) + 'scat'.rjust(8)   + 'closest'.rjust(11)   +  'in lab'.rjust(8) + 'c.o.m.'.rjust(15)        + 'laboratory'.rjust(15)    +  'lab/c.o.m.'.rjust(15)    + 'scat'.rjust(8) + 'rec'.rjust(8)  + 'difference'.rjust(16),
-        'lab  '.rjust(6) + 'lab  '.rjust(8) + 'c.o.m.'.rjust(8) + 'appr (fm)'.rjust(11) +  '(MeV)'.rjust(8)  + '(mb/sr)'.rjust(15)       + '(mb/sr)'.rjust(15)       +  '(1)'.rjust(15)           + '(1)'.rjust(8)  + '(1)'.rjust(8)  + '(ns)'.rjust(16)
+        'theta'.rjust(6) + 'theta'.rjust(8) + 'theta'.rjust(8)  + 'dist of'.rjust(11)   +  'path'.rjust(10) + 'energy'.rjust(8) + 'energy'.rjust(8) + 'dsigma/domega'.rjust(15) + 'dsigma/domega'.rjust(15) +  'dsigma/domega'.rjust(15) + 'beta'.rjust(8) + 'beta'.rjust(8) + 'time of flight'.rjust(16),
+        'scat '.rjust(6) + 'rec  '.rjust(8) + 'scat'.rjust(8)   + 'closest'.rjust(11)   +  'length'.rjust(10) + 'loss'.rjust(8) + 'in lab'.rjust(8) + 'c.o.m.'.rjust(15)        + 'laboratory'.rjust(15)    +  'lab/c.o.m.'.rjust(15)    + 'scat'.rjust(8) + 'rec'.rjust(8)  + 'difference'.rjust(16),
+        'lab  '.rjust(6) + 'lab  '.rjust(8) + 'c.o.m.'.rjust(8) + 'appr (fm)'.rjust(11) +  '(mg/cm^2)'.rjust(10) + '(MeV)'.rjust(8) + '(MeV)'.rjust(8)  + '(mb/sr)'.rjust(15)       + '(mb/sr)'.rjust(15)       +  '(1)'.rjust(15)           + '(1)'.rjust(8)  + '(1)'.rjust(8)  + '(ns)'.rjust(16)
          ]
+
+print "debugging:"
+stopping_power, energy_loss = run_elast(6,12,z1,a1,e0,0.110)
+print stopping_power, energy_loss
+print e0 - energy_loss
 
 print 'SCATTERED particle:'
 for line in header:
@@ -101,6 +163,23 @@ for theta in numpy.linspace(amin, amax, int((amax - amin) / astep) + 1): # do 5 
     thetb = (180.-tcm)/2.
     ruthc = ruthi/math.sin(math.radians(tcm/2.))**4
     e1 = e0*(1.-2.*am*(1.-math.cos(math.radians(tcm))))
+    e_recoil = e0 - e1
+
+    # If the user specified a target thickness, adjust beta and delta-t.
+    if not target_thickness == None:
+        # e0 has already been adjusted to the energy at the center of the target.
+        path_length = target_thickness / 2. / math.cos(math.radians(theta))
+        recoil_path_length = target_thickness / 2. / math.cos(math.radians(thetb))
+        stopping_power, energy_loss = run_elast(z2,a2,z1,a1,e1,path_length)
+        recoil_stopping_power, recoil_energy_loss = run_elast(z2,a2,z2,a2,e0 - e1,recoil_path_length)
+        e1 = e1 - energy_loss
+        e_recoil = e_recoil - recoil_energy_loss
+    else:
+        # Do not calculate any energy loss after the interaction.
+        path_length = 0.
+        recoil_path_length = 0.
+        energy_loss = 0.
+
     ratio = math.sin(math.radians(tcm))**2/(math.cos(math.radians(tcm-theta)) *math.sin(math.radians(theta))**2)
     ruthl = ruthc*ratio
     thed1 =  const1*math.cos(math.radians(theta)) + const2*math.sin(math.radians(theta))
@@ -110,7 +189,7 @@ for theta in numpy.linspace(amin, amax, int((amax - amin) / astep) + 1): # do 5 
     dis1 = disf/ thed1
     dis2 = disf/ thed2
     beta1 = .046*math.sqrt(e1/a1)
-    beta2 = .046*math.sqrt((e0-e1)/a2)
+    beta2 = .046*math.sqrt((e_recoil)/a2)
     tim_s = .0333564*dis1/beta1
     tim_r = .0333564*dis2/beta2
     timdi = tim_s-tim_r
@@ -121,7 +200,7 @@ for theta in numpy.linspace(amin, amax, int((amax - amin) / astep) + 1): # do 5 
 
     dca = l*(1.0+(1.0/math.sin(math.radians(tcm/2.0))))
 
-    print '{0:6.2f}{1:8.2f}{2:8.2f}{3:11.2f}{4:8.2f}{5:15.2e}{6:15.2e}{7:15.2e}{8:8.3f}{9:8.3f}{10:16.2f}'.format(theta,thetb,tcm,dca,e1,ruthc,ruthl,ratio,beta1,beta2,timdi)
+    print '{0:6.2f}{1:8.2f}{2:8.2f}{3:11.2f}{4:10.3f}{5:8.1f}{6:8.2f}{7:15.2e}{8:15.2e}{9:15.2e}{10:8.3f}{11:8.3f}{12:16.2f}'.format(theta,thetb,tcm,dca,path_length,energy_loss,e1,ruthc,ruthl,ratio,beta1,beta2,timdi)
 
 
 if thelim < 179.0:
@@ -163,9 +242,9 @@ if thelim < 179.0:
 
 # Recoiling target.
 header = [
-        'theta'.rjust(6) + 'theta'.rjust(8) + 'theta'.rjust(8)  + 'dist of'.rjust(11)   +  'energy'.rjust(8) + 'dsigma/domega'.rjust(15) + 'dsigma/domega'.rjust(15) +  'dsigma/domega'.rjust(15) + 'beta'.rjust(8) + 'beta'.rjust(8) + 'time of flight'.rjust(16),
-        'rec'.rjust(6) + 'scat'.rjust(8) + 'rec'.rjust(8)   + 'closest'.rjust(11)   +  'in lab'.rjust(8) + 'c.o.m.'.rjust(15)        + 'laboratory'.rjust(15)    +  'lab/c.o.m.'.rjust(15)    + 'scat'.rjust(8) + 'rec'.rjust(8)  + 'difference'.rjust(16),
-        'lab  '.rjust(6) + 'lab  '.rjust(8) + 'c.o.m.'.rjust(8) + 'appr (fm)'.rjust(11) +  '(MeV)'.rjust(8)  + '(mb/sr)'.rjust(15)       + '(mb/sr)'.rjust(15)       +  '(1)'.rjust(15)           + '(1)'.rjust(8)  + '(1)'.rjust(8)  + '(ns)'.rjust(16)
+        'theta'.rjust(6) + 'theta'.rjust(8) + 'theta'.rjust(8)  + 'dist of'.rjust(11)  +  'path'.rjust(10) + 'energy'.rjust(8)  +  'energy'.rjust(8) + 'dsigma/domega'.rjust(15) + 'dsigma/domega'.rjust(15) +  'dsigma/domega'.rjust(15) + 'beta'.rjust(8) + 'beta'.rjust(8) + 'time of flight'.rjust(16),
+        'rec'.rjust(6) + 'scat'.rjust(8) + 'rec'.rjust(8)   + 'closest'.rjust(11)    +  'length'.rjust(10)  + 'loss'.rjust(8)  +  'in lab'.rjust(8) + 'c.o.m.'.rjust(15)        + 'laboratory'.rjust(15)    +  'lab/c.o.m.'.rjust(15)    + 'rec'.rjust(8) + 'scat'.rjust(8)  + 'difference'.rjust(16),
+        'lab  '.rjust(6) + 'lab  '.rjust(8) + 'c.o.m.'.rjust(8) + 'appr (fm)'.rjust(11) +  '(mg/cm^2)'.rjust(10)  + '(MeV)'.rjust(8) +  '(MeV)'.rjust(8)  + '(mb/sr)'.rjust(15)       + '(mb/sr)'.rjust(15)       +  '(1)'.rjust(15)           + '(1)'.rjust(8)  + '(1)'.rjust(8)  + '(ns)'.rjust(16)
          ]
 
 print 'RECOILING particle:'
@@ -175,11 +254,26 @@ for line in header:
 
 for theta in numpy.linspace(amin, min(amax,89.), int((min(amax,89.) - amin) / astep) + 1):
     # theta is now RECOIL angle.
-    e2 = e0*4.*am*math.cos(math.radians(theta))**2
+    e_recoil = e0*4.*am*math.cos(math.radians(theta))**2
+    e_projectile = e0 - e_recoil
     tcm=math.degrees(math.acos(1.-2.*math.cos(math.radians(theta))**2))
     thetb=math.degrees(math.atan(math.sin(math.radians(2.*theta)) /(a1/a2-math.cos(2.*math.radians(theta)))))
     if thetb < 0.0:
         thetb=180.+thetb
+    # If the user specified a target thickness, adjust beta and delta-t.
+    if not target_thickness == None:
+        # e0 has already been adjusted to the energy at the center of the target.
+        recoil_path_length = target_thickness / 2. / math.cos(math.radians(theta))
+        projectile_path_length = target_thickness / 2. / math.cos(math.radians(thetb))
+        recoil_stopping_power, recoil_energy_loss = run_elast(z2,a2,z2,a2,e_recoil,recoil_path_length)
+        projectile_stopping_power, projectile_energy_loss = run_elast(z2,a2,z1,a1,e1,projectile_path_length)
+        e_recoil = e_recoil - recoil_energy_loss
+        e_projectile = e_projectile - projectile_energy_loss
+    else:
+        # Do not calculate any energy loss after the interaction.
+        projectile_path_length = 0.
+        recoil_path_length = 0.
+
     ruthc=ruthi/math.sin(math.radians(tcm/2.))**4
     ratio=4.*math.cos(math.radians(theta))
     ruthl=ruthc*ratio
@@ -190,15 +284,15 @@ for theta in numpy.linspace(amin, min(amax,89.), int((min(amax,89.) - amin) / as
         thed1= consta*math.cos(math.radians(thetb)) + constb*math.sin(math.radians(thetb))
     dis1=disf/ thed1
     dis2=disf/ thed2
-    beta1=.046*math.sqrt(e2/a2)
-    beta2=.046*math.sqrt((e0-e2)/a1)
+    beta1=.046*math.sqrt(e_recoil/a2)
+    beta2=.046*math.sqrt((e_projectile)/a1)
     tim_s=.0333564*dis1/beta1
     tim_r=.0333564*dis2/beta2
     timdi=tim_s-tim_r
     tcmscat=thetb+math.degrees(math.asin(a1*math.sin(math.radians(thetb))/a2))
     dca=l*(1.0+(1.0/math.sin(math.radians(tcmscat/2.0))))
 
-    print '{0:6.2f}{1:8.2f}{2:8.2f}{3:11.2f}{4:8.2f}{5:15.2e}{6:15.2e}{7:15.2e}{8:8.3f}{9:8.3f}{10:16.2f}'.format(theta,thetb,tcm,dca,e1,ruthc,ruthl,ratio,beta2,beta1,timdi)
+    print '{0:6.2f}{1:8.2f}{2:8.2f}{3:11.2f}{4:10.3f}{5:8.1f}{6:8.2f}{7:15.2e}{8:15.2e}{9:15.2e}{10:8.3f}{11:8.3f}{12:16.2f}'.format(theta,thetb,tcm,dca,recoil_path_length,recoil_energy_loss,e_recoil,ruthc,ruthl,ratio,beta1,beta2,timdi)
 
     # Add to lists for delta-TOF plot:
     recoil_theta_list.append(theta)
